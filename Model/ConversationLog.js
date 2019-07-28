78403008e4d21c20c8e32ddc86c98badc4da27d8
Tobias Hartker <thartker@techfak.uni-bielefeld.de>
2019-07-28 16:19:03 +0200

    Fixes typos.

--- Model/ConversationLog.js
+++ Model/ConversationLog.js
@@ -2,9 +2,11 @@
  * A simple conversation log, to gather everthing needed for evaluations.
  */
 class ConversationLog {
-    constructor(Id, userId, transcript){
+    constructor(Id, userId, transcript) {
         this.Id = Id;
         this.userId = userId;
         this.transcript = transcript;
     }
 }
+
+module.exports.ConversationLog = ConversationLog;
