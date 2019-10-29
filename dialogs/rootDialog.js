461316622d37aed7e1e86d9a978bc25a1ddde2b6
k4l4x <things_went_south@icloud.com>
2019-10-29 01:04:13 +0100

    Return normal behavior to /start command.

--- dialogs/rootDialog.js
+++ dialogs/rootDialog.js
@@ -184,7 +184,8 @@ class RootDialog extends ComponentDialog {
         let options = {};
         const message = step.context.activity.text.toLowerCase();
 
-        if (message.includes(validMessages.START)) {
+        if (message.includes(validMessages.START) &&
+            conversationData.promptedStudy === false) {
             console.log('[RootDialog]: user first contact');
             dialogId = WELCOME_DIALOG;
         } else if (message.includes(validMessages.FIND_DISH)) {
